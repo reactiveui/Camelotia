68643d3a647a3216eff610d7e998f9c79916ba09
artyom <worldbeater-dev@yandex.ru>
2018-12-14 12:00:00 +0300

    Increased coverage

---
+++ Camelotia.Services.Tests/LocalFileSystemProviderTests.cs
@@ -0,0 +1,38 @@
+using System.IO;
+using System.Threading.Tasks;
+using Camelotia.Services.Interfaces;
+using Camelotia.Services.Providers;
+using FluentAssertions;
+using Xunit;
+
+namespace Camelotia.Services.Tests
+{
+    public sealed class LocalFileSystemProviderTests
+    {
+        private static readonly string Separator = Path.DirectorySeparatorChar.ToString();
+        private readonly IProvider _provider = new LocalFileSystemProvider();
+        
+        [Fact]
+        public async Task LocalFileSystemShouldNotSupportAuth()
+        {
+            _provider.SupportsDirectAuth.Should().BeFalse();
+            _provider.SupportsOAuth.Should().BeFalse();
+            await _provider.DirectAuth(string.Empty, string.Empty);
+            await _provider.OAuth();
+        }
+
+        [Fact]
+        public async Task ShouldReturnFilesFromSpecificPath()
+        {
+            var real = await _provider.Get(Separator);
+            var expected = Directory.GetFileSystemEntries(Separator);
+            foreach (var model in real)
+                expected.Should().Contain(path =>
+                    model.Path == path &&
+                    model.Name == Path.GetFileName(path) && 
+                    model.IsFolder == File
+                        .GetAttributes(path)
+                        .HasFlag(FileAttributes.Directory));
+        }
+    }
+}
---
+++ Camelotia.Services.Tests/ProviderStorageTests.cs
@@ -0,0 +1,22 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Camelotia.Services.Providers;
+using Xunit;
+
+namespace Camelotia.Services.Tests
+{
+    public sealed class ProviderStorageTests
+    {
+        [Fact]
+        public async Task ShouldResolveAllSupportedProviders()
+        {
+            var provider = new ProviderStorage();
+            var sequence = await provider.LoadProviders();
+            var providers = sequence.ToList();
+            
+            Assert.Contains(providers, x => x is LocalFileSystemProvider);
+            Assert.Contains(providers, x => x is VkontakteFileSystemProvider);
+            Assert.Contains(providers, x => x is YandexFileSystemProvider);
+        }
+    }
+}
