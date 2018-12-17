a6a2bcd0a89a9d602ff84aef0920a55d65fd14e9
Greg Amidon <gamidon@gmail.com>
2018-12-17 07:45:49 -0600

    Added Tests for InitialPath on the IProvider implementers.

---
+++ Camelotia.Presentation.Tests/VkontakteFileSystemProviderTests.cs
@@ -0,0 +1,18 @@
+﻿using Camelotia.Services.Interfaces;
+using Camelotia.Services.Providers;
+using FluentAssertions;
+using Xunit;
+
+namespace Camelotia.Presentation.Tests
+{
+    public sealed class VkontakteFileSystemProviderTests
+    {
+        private readonly IProvider _provider = new VkontakteFileSystemProvider();
+
+        [Fact]
+        public void ShouldImplementNonNullInitialPath()
+        {
+            _provider.InitialPath.Should().NotBeNull();
+        }
+    }
+}
---
+++ Camelotia.Presentation.Tests/YandexFileSystemProviderTests.cs
@@ -0,0 +1,18 @@
+﻿using Camelotia.Services.Interfaces;
+using Camelotia.Services.Providers;
+using FluentAssertions;
+using Xunit;
+
+namespace Camelotia.Presentation.Tests
+{
+    public sealed class YandexFileSystemProviderTests
+    {
+        private readonly IProvider _provider = new YandexFileSystemProvider();
+
+        [Fact]
+        public void ShouldImplementNonNullInitialPath()
+        {
+            _provider.InitialPath.Should().NotBeNull();
+        }
+    }
+}
