29058110999ba863ad44cdb3a587e76fad8dd3ec
Artjom Graf <prizrak.nra@gmail.com>
2018-12-31 01:27:13 +0300

    Cleanup

--- Camelotia.Presentation.Xamarin/Services/XamarinAuthenticator.cs
+++ Camelotia.Presentation.Xamarin/Services/XamarinAuthenticator.cs
@@ -7,12 +7,9 @@ namespace Camelotia.Presentation.Xamarin.Services
 {
     public sealed class XamarinAuthenticator : IAuthenticator
     {
-        public YandexAuthenticationType YandexAuthenticationType => throw new NotImplementedException();
+        public YandexAuthenticationType YandexAuthenticationType => YandexAuthenticationType.Token;
 
-        public Task<string> ReceiveYandexCode(Uri uri, IPAddress address, int port)
-        {
-            throw new NotImplementedException();
-        }
+        public Task<string> ReceiveYandexCode(Uri uri, IPAddress address, int port) => throw new PlatformNotSupportedException();
 
         public Task<string> ReceiveYandexToken(Uri uri)
         {
