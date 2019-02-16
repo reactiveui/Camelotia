7425a2021dcdc7eb301523868a816d1ec837daf4
Artjom Graf <prizrak.nra@gmail.com>
2019-02-16 22:26:12 +0300

    Implement create folder page

--- Camelotia.Presentation.Xamarin/Views/CreateFolderView.xaml.cs
+++ Camelotia.Presentation.Xamarin/Views/CreateFolderView.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Camelotia.Presentation.Interfaces;
 using ReactiveUI;
 using ReactiveUI.XamForms;
+using System.Reactive.Disposables;
 using Xamarin.Forms.Xaml;
 
 namespace Camelotia.Presentation.Xamarin.Views
@@ -11,9 +12,19 @@ namespace Camelotia.Presentation.Xamarin.Views
         public CreateFolderView()
         {
             InitializeComponent();
-            this.WhenActivated(disposables => 
+            this.WhenActivated(disposables =>
             {
+                this.BindCommand(ViewModel, x => x.Create, x => x.CreateFolderButton)
+                    .DisposeWith(disposables);
+                this.BindCommand(ViewModel, x => x.Close, x => x.CloseButton)
+                    .DisposeWith(disposables);
 
+                this.Bind(ViewModel, x => x.Name, x => x.NameEntry.Text)
+                    .DisposeWith(disposables);
+                this.OneWayBind(ViewModel, x => x.ErrorMessage, x => x.ErrorLabel.Text)
+                    .DisposeWith(disposables);
+                this.OneWayBind(ViewModel, x => x.HasErrors, x => x.ErrorLabel.IsVisible)
+                    .DisposeWith(disposables);
             });
         }
     }
