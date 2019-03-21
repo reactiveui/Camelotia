6454e04d02e180b6fb3d591564137a16cffaf702
Artjom Graf <prizrak.nra@gmail.com>
2019-03-22 00:43:25 +0300

    Awesome icons!

--- Camelotia.Presentation.Xamarin/Views/ProviderExplorerFileView.xaml.cs
+++ Camelotia.Presentation.Xamarin/Views/ProviderExplorerFileView.xaml.cs
@@ -2,6 +2,7 @@
 using ReactiveUI;
 using ReactiveUI.XamForms;
 using System.Reactive.Disposables;
+using System.Reactive.Linq;
 using Xamarin.Forms.Xaml;
 
 namespace Camelotia.Presentation.Xamarin.Views
@@ -19,6 +20,11 @@ namespace Camelotia.Presentation.Xamarin.Views
                 this.OneWayBind(ViewModel, x => x.Modified, x => x.ModifiedLabel.Text)
                     .DisposeWith(disposables);
                 this.OneWayBind(ViewModel, x => x.Size, x => x.SizeLabel.Text)
+                    .DisposeWith(disposables);
+
+                this.WhenAnyValue(x => x.ViewModel.IsFolder)
+                    .Select(folder => folder ? "fas-folder" : "fas-file")
+                    .BindTo(this, x => x.IconImage.Icon)
                     .DisposeWith(disposables);
             });
         }
