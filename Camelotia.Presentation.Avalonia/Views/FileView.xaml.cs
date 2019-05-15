de6186b3e2e85dfdea2d9787daeb454e0e92cd3e
Artjom Graf <prizrak.nra@gmail.com>
2019-05-15 20:56:30 +0300

    Avalonia fixes

--- Camelotia.Presentation.Avalonia/Views/FileView.xaml.cs
+++ Camelotia.Presentation.Avalonia/Views/FileView.xaml.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 using Avalonia.ReactiveUI;
 using Camelotia.Presentation.Interfaces;
@@ -9,7 +13,17 @@ namespace Camelotia.Presentation.Avalonia.Views
     {
         public FileView()
         {
-            this.WhenActivated(disposables => { });
+            this.WhenActivated(disposables =>
+            {
+                Observable.FromEventPattern<PointerReleasedEventArgs>(
+                        handler => PointerReleased += handler,
+                        handler => PointerReleased -= handler)
+                    .Where(args => args.EventArgs.MouseButton == MouseButton.Right)
+                    .Select(args => (FileView)args.Sender)
+                    .Select(element => element.ViewModel)
+                    .Subscribe(file => file.Provider.SelectedFile = file)
+                    .DisposeWith(disposables);
+            });
             AvaloniaXamlLoader.Load(this);
         }
     }
