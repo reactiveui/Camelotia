47f03163fe63eec1255855ac2f776a0b56bda1fd
Artjom Graf <prizrak.nra@gmail.com>
2019-05-17 11:39:21 +0300

    Double tap support on Avalonia
    Currently broken

--- Camelotia.Presentation.Avalonia/Views/FileView.xaml.cs
+++ Camelotia.Presentation.Avalonia/Views/FileView.xaml.cs
@@ -1,7 +1,9 @@
 using System;
 using System.ComponentModel;
+using System.Reactive;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using Avalonia.ReactiveUI;
 using Camelotia.Presentation.Interfaces;
@@ -20,6 +22,13 @@ namespace Camelotia.Presentation.Avalonia.Views
                         handler => ContextMenu.ContextMenuOpening -= handler)
                     .Do(args => args.EventArgs.Cancel = false)
                     .Subscribe(args => ViewModel.Provider.SelectedFile = ViewModel)
+                    .DisposeWith(disposables);
+
+                Observable.FromEventPattern<RoutedEventArgs>(
+                        handler => DoubleTapped += handler,
+                        handler => DoubleTapped -= handler)
+                    .Select(args => Unit.Default)
+                    .InvokeCommand(this, x => x.ViewModel.Provider.Open)
                     .DisposeWith(disposables);
             });
             AvaloniaXamlLoader.Load(this);
