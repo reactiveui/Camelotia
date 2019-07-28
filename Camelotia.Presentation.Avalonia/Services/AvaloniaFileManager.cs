ff40975cbb6e56271cc363587f585e290c79494b
Artjom Graf <prizrak.nra@gmail.com>
2019-07-28 13:21:19 +0300

    Initial attempt

--- Camelotia.Presentation.Avalonia/Services/AvaloniaFileManager.cs
+++ Camelotia.Presentation.Avalonia/Services/AvaloniaFileManager.cs
@@ -2,7 +2,6 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Avalonia;
 using Avalonia.Controls;
 using Camelotia.Services.Interfaces;
 
@@ -10,10 +9,14 @@ namespace Camelotia.Presentation.Avalonia.Services
 {
     public sealed class AvaloniaFileManager : IFileManager
     {
+        private readonly Window _window;
+
+        public AvaloniaFileManager(Window window) => _window = window;
+
         public async Task<Stream> OpenWrite(string name)
         {
             var fileDialog = new OpenFolderDialog();
-            var folder = await fileDialog.ShowAsync(Application.Current.MainWindow);
+            var folder = await fileDialog.ShowAsync(_window);
             var path = Path.Combine(folder, name);
             return File.Create(path);
         }
@@ -21,7 +24,7 @@ namespace Camelotia.Presentation.Avalonia.Services
         public async Task<(string Name, Stream Stream)> OpenRead()
         {
             var fileDialog = new OpenFileDialog {AllowMultiple = false};
-            var files = await fileDialog.ShowAsync(Application.Current.MainWindow);
+            var files = await fileDialog.ShowAsync(_window);
             var path = files.First();
             
             var attributes = File.GetAttributes(path);
