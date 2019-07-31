1bf6f38c99a53f15a68edd5fe211a304630db8fd
artyom <worldbeater-dev@yandex.ru>
2019-07-31 14:03:23 +0300

    Include Xamarin and UAP targets in build

--- build/Build.cs
+++ build/Build.cs
@@ -1,13 +1,13 @@
-using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices;
 using Nuke.Common;
 using Nuke.Common.Execution;
 using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.MSBuild;
 using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
+using static Nuke.Common.Tools.MSBuild.MSBuildTasks;
 
 // ReSharper disable ArrangeTypeMemberModifiers
 
@@ -15,95 +15,92 @@ using static Nuke.Common.Tools.DotNet.DotNetTasks;
 [UnsetVisualStudioEnvironmentVariables]
 internal class Build : NukeBuild
 {
+    private const string InteractiveProjectName = "Camelotia.Presentation.Avalonia";
     private const string CoverageFileName = "coverage.cobertura.xml";
-    private static readonly IReadOnlyList<string> CrossPlatformProjects = new List<string>
-    {
-        "Camelotia.Presentation.Tests.csproj",
-        "Camelotia.Presentation.Avalonia.csproj"
-    };
-    
-    private static readonly IReadOnlyList<string> WindowsOnlyProjects = new List<string>
-    {
-        "Camelotia.Presentation.Xamarin.csproj",
-        "Camelotia.Presentation.Uwp.csproj"
-    };
-    
+
     public static int Main() => Execute<Build>(x => x.Run);
     
     [Parameter("Compilation configuration, can either be Debug or Release.")] 
     public readonly string Configuration = IsLocalBuild ? "Debug" : "Release";
-
-    [Parameter("If interactive is set to true, then a GUI launches.")]
-    public readonly bool Interactive = false;
+    
+    [Parameter("If interactive is set to true, a GUI launches.")]
+    public readonly bool Interactive;
+    
+    [Parameter("If full is set to true, then all projects will build.")]
+    public readonly bool Full;
 
     AbsolutePath SourceDirectory => RootDirectory / "src";
-
     AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
 
     Target Clean => _ => _
-        .Before(Restore)
+        .Before(Test)
         .Executes(() => SourceDirectory
             .GlobDirectories("**/bin", "**/obj", "**/artifacts")
             .ForEach(DeleteDirectory));
-
-    Target Restore => _ => _
-        .Executes(() => SourceDirectory
-            .GlobFiles("**/*.csproj")
-            .Where(SupportedByEnvironment)
-            .ForEach(path =>
-                DotNetRestore(s => s
-                    .SetProjectFile(path))));
-
-    Target Compile => _ => _
-        .DependsOn(Restore)
-        .Executes(() => SourceDirectory
-            .GlobFiles("**/*.csproj")
-            .Where(SupportedByEnvironment)
-            .ForEach(path =>
-                DotNetBuild(s => s
-                    .SetProjectFile(path)
-                    .SetConfiguration(Configuration)
-                    .EnableNoRestore())));
-
+    
     Target Test => _ => _
-        .DependsOn(Compile)
         .Executes(() => SourceDirectory
             .GlobFiles("**/*.Tests.csproj")
-            .Where(SupportedByEnvironment)
-            .ForEach(path => 
-                DotNetTest(s => s
+            .ForEach(path =>
+                DotNetTest(settings => settings
                     .SetProjectFile(path)
                     .SetConfiguration(Configuration)
-                    .EnableNoBuild()
-                    .EnableNoRestore()
                     .SetLogger($"trx;LogFileName={ArtifactsDirectory / "report.trx"}")
                     .AddProperty("CollectCoverage", true)
                     .AddProperty("CoverletOutputFormat", "cobertura")
                     .AddProperty("Exclude", "[xunit.*]*")
                     .AddProperty("CoverletOutput", ArtifactsDirectory / CoverageFileName))));
 
-    Target Run => _ => _
+    Target CompileAvaloniaApp => _ => _
+        .DependsOn(Test)
+        .Executes(() => SourceDirectory
+            .GlobFiles("**/*.Avalonia.csproj")
+            .ForEach(path =>
+                DotNetBuild(settings => settings
+                    .SetProjectFile(path)
+                    .SetConfiguration(Configuration))));
+
+    private Target CompileUniversalWindowsApp => _ => _
         .DependsOn(Test)
         .Executes(() =>
         {
-            if (!Interactive) return;
-            var runnable = SourceDirectory
-                .GlobFiles("**/*.Avalonia.csproj")
-                .First();
-
-            DotNetRun(s => s
-                .SetProjectFile(runnable)
+            var execute = EnvironmentInfo.IsWin && Full;
+            Logger.Normal($"Should compile for Universal Windows: {execute}");
+            if (!execute) return;
+            
+            var project = SourceDirectory.GlobFiles("**/*.Uwp.csproj").First();
+            MSBuild(settings => settings
+                .SetProjectFile(project)
                 .SetConfiguration(Configuration)
-                .EnableNoBuild()
-                .EnableNoRestore());
+                .SetTargetPlatform(MSBuildTargetPlatform.x86));
         });
 
-    bool SupportedByEnvironment(AbsolutePath path)
-    {
-        var projects = CrossPlatformProjects.ToList();
-        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-            projects.AddRange(WindowsOnlyProjects);
-        
-        return projects.Any(name => path.ToString().Contains(name));
-    }
+    Target CompileXamarinAndroidApp => _ => _
+        .DependsOn(Test)
+        .Executes(() =>
+        {
+            var execute = EnvironmentInfo.IsWin && Full;
+            Logger.Normal($"Should compile for Android: {execute}");
+            if (!execute) return;
+            
+            var project = SourceDirectory.GlobFiles("**/*.Xamarin.Android.csproj").First();
+            MSBuild(settings => settings
+                .SetProjectFile(project)
+                .SetConfiguration(Configuration)
+                .SetTargetPlatform(MSBuildTargetPlatform.x86));
+        });
+
+    Target Run => _ => _
+        .DependsOn(CompileAvaloniaApp)
+        .DependsOn(CompileUniversalWindowsApp)
+        .DependsOn(CompileXamarinAndroidApp)
+        .Executes(() => SourceDirectory
+            .GlobFiles($"**/{InteractiveProjectName}.csproj")
+            .Where(x => Interactive)
+            .ForEach(path => 
+                DotNetRun(settings => settings
+                    .SetProjectFile(path)
+                    .SetConfiguration(Configuration)
+                    .EnableNoRestore()
+                    .EnableNoBuild())));
 }
