62c9c25bf56395ce1aadcb31d398b47d009411b9
Artjom Graf <prizrak.nra@gmail.com>
2020-08-15 15:19:14 +0300

    Support suspension in UWP

--- src/Camelotia.Presentation.Uwp/Bootstrapper.cs
+++ src/Camelotia.Presentation.Uwp/Bootstrapper.cs
@@ -1,14 +1,10 @@
 ﻿using Camelotia.Presentation.Interfaces;
 using Camelotia.Presentation.Uwp.Services;
 using Camelotia.Presentation.ViewModels;
-using Camelotia.Services.Interfaces;
-using Camelotia.Services.Providers;
-using Camelotia.Services.Storages;
 using Camelotia.Services.Models;
-using System.Reactive.Concurrency;
-using System.Collections.Generic;
 using ReactiveUI;
-using System;
+using Camelotia.Services;
+using Camelotia.Presentation.AppState;
 
 namespace Camelotia.Presentation.Uwp
 {
@@ -16,34 +12,25 @@ namespace Camelotia.Presentation.Uwp
     {
         public static IMainViewModel BuildMainViewModel()
         {
-            Akavache.BlobCache.ApplicationName = "Camelotia";
-            var cache = Akavache.BlobCache.UserAccount;
-            var login = new UniversalWindowsYandexAuthenticator();
-            var files = new UniversalWindowsFileManager();
-
             return new MainViewModel(
-                (provider, auth) => new ProviderViewModel(
-                    model => new CreateFolderViewModel(model, provider),
-                    model => new RenameFileViewModel(model, provider),
-                    (file, model) => new FileViewModel(model, file),
-                    auth, files, provider
+                RxApp.SuspensionHost.GetAppState<MainState>(),
+                new ProviderFactory(
+                    new UniversalWindowsYandexAuthenticator(), 
+                    Akavache.BlobCache.UserAccount, 
+                    new[] { ProviderType.Yandex, ProviderType.VkDocs, ProviderType.Ftp, ProviderType.Sftp, ProviderType.GitHub }
                 ),
-                provider => new AuthViewModel(
-                    new DirectAuthViewModel(provider),
-                    new HostAuthViewModel(provider),
-                    new OAuthViewModel(provider),
+                (state, provider) => new ProviderViewModel(
+                    owner => new CreateFolderViewModel(state.CreateFolderState, owner, provider),
+                    owner => new RenameFileViewModel(state.RenameFileState, owner, provider),
+                    (file, owner) => new FileViewModel(owner, file),
+                    new AuthViewModel(
+                        new DirectAuthViewModel(state.AuthState.DirectAuthState, provider),
+                        new HostAuthViewModel(state.AuthState.HostAuthState, provider),
+                        new OAuthViewModel(provider),
+                        provider
+                    ),
+                     new UniversalWindowsFileManager(),
                     provider
-                ),
-                new AkavacheStorage(
-                    new Dictionary<string, Func<ProviderModel, IProvider>>
-                    {
-                        ["Yandex Disk"] = id => new YandexDiskProvider(id, login, cache),
-                        ["Vkontakte Docs"] = id => new VkDocsProvider(id, cache),
-                        ["FTP"] = id => new FtpProvider(id),
-                        ["SFTP"] = id => new SftpProvider(id),
-                        ["GitHub"] = id => new GitHubProvider(id, cache)
-                    },
-                    cache
                 )
             );
         }
