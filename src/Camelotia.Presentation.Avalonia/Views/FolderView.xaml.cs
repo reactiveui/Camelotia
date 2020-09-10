dea404d7d3ac154d531b7ffe374cd61176a38278
Markus K <13160892+MarkusKgit@users.noreply.github.com>
2020-09-10 16:16:05 +0200

    Fix naming and formatting

--- src/Camelotia.Presentation.Avalonia/Views/FolderView.xaml.cs
+++ src/Camelotia.Presentation.Avalonia/Views/FolderView.xaml.cs
@@ -16,9 +16,8 @@ namespace Camelotia.Presentation.Avalonia.Views
     {
         public MenuItem TopLevelMenu => this.FindControl<MenuItem>("TopLevelMenu");
 
-        public DrawingPresenter ArrowDrawingRight => this.FindControl<DrawingPresenter>("ArrowDrawingRight");
-        public DrawingPresenter ArrowDrawingDown => this.FindControl<DrawingPresenter>("ArrowDrawingDown");
-
+        public DrawingPresenter ArrowRight => this.FindControl<DrawingPresenter>("ArrowRight");
+        public DrawingPresenter ArrowDown => this.FindControl<DrawingPresenter>("ArrowDown");
 
         public FolderView()
         {
@@ -28,8 +27,8 @@ namespace Camelotia.Presentation.Avalonia.Views
                 this.WhenAnyValue(x => x.TopLevelMenu.IsSubMenuOpen)
                     .Do(isOpen =>
                     {
-                        ArrowDrawingRight.IsVisible = !isOpen;
-                        ArrowDrawingDown.IsVisible = isOpen;
+                        ArrowRight.IsVisible = !isOpen;
+                        ArrowDown.IsVisible = isOpen;
                     })
                     .Subscribe()
                     .DisposeWith(disposables);                
