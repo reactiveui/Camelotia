aa5c13ca83b7c0e6758a45de7296189f8ac734b9
Artyom <worldbeater-dev@yandex.ru>
2020-10-24 00:32:12 +0300

    Validations for Xamarin.Forms

--- src/Camelotia.Presentation.Xamarin/Views/HostAuthView.xaml.cs
+++ src/Camelotia.Presentation.Xamarin/Views/HostAuthView.xaml.cs
@@ -1,5 +1,9 @@
-﻿using Camelotia.Presentation.Interfaces;
+﻿using System;
+using System.Reactive.Disposables;
+using Camelotia.Presentation.Interfaces;
 using ReactiveUI;
+using ReactiveUI.Validation.Extensions;
+using ReactiveUI.Validation.Formatters;
 using ReactiveUI.XamForms;
 using Xamarin.Forms.Xaml;
 
@@ -11,7 +15,35 @@ namespace Camelotia.Presentation.Xamarin.Views
         public HostAuthView()
         {
             InitializeComponent();
-            this.WhenActivated(disposables => { });
+            this.WhenActivated(disposables =>
+            {
+                this.BindValidation(ViewModel, x => x.Address, x => x.HostNameErrorLabel.Text)
+                    .DisposeWith(disposables);
+                this.BindValidation(ViewModel, x => x.Port, x => x.PortErrorLabel.Text)
+                    .DisposeWith(disposables);
+                this.BindValidation(ViewModel, x => x.Username, x => x.UserNameErrorLabel.Text)
+                    .DisposeWith(disposables);
+                this.BindValidation(ViewModel, x => x.Password, x => x.PasswordErrorLabel.Text)
+                    .DisposeWith(disposables);
+                this.BindValidation(ViewModel, x => x.FormErrorLabel.Text, new SingleLineFormatter(Environment.NewLine))
+                    .DisposeWith(disposables);
+
+                this.WhenAnyValue(x => x.HostNameErrorLabel.Text, text => !string.IsNullOrWhiteSpace(text))
+                    .BindTo(this, x => x.HostNameErrorLabel.IsVisible)
+                    .DisposeWith(disposables);
+                this.WhenAnyValue(x => x.PortErrorLabel.Text, text => !string.IsNullOrWhiteSpace(text))
+                    .BindTo(this, x => x.PortErrorLabel.IsVisible)
+                    .DisposeWith(disposables);
+                this.WhenAnyValue(x => x.UserNameErrorLabel.Text, text => !string.IsNullOrWhiteSpace(text))
+                    .BindTo(this, x => x.UserNameErrorLabel.IsVisible)
+                    .DisposeWith(disposables);
+                this.WhenAnyValue(x => x.PasswordErrorLabel.Text, text => !string.IsNullOrWhiteSpace(text))
+                    .BindTo(this, x => x.PasswordErrorLabel.IsVisible)
+                    .DisposeWith(disposables);
+                this.WhenAnyValue(x => x.FormErrorLabel.Text, text => !string.IsNullOrWhiteSpace(text))
+                    .BindTo(this, x => x.FormErrorLabel.IsVisible)
+                    .DisposeWith(disposables);
+            });
         }
     }
 }
--- src/Camelotia.Presentation.Xamarin/Views/RenameFileView.xaml.cs
+++ src/Camelotia.Presentation.Xamarin/Views/RenameFileView.xaml.cs
@@ -1,5 +1,9 @@
-﻿using Camelotia.Presentation.Interfaces;
+﻿using System;
+using System.Reactive.Disposables;
+using Camelotia.Presentation.Interfaces;
 using ReactiveUI;
+using ReactiveUI.Validation.Extensions;
+using ReactiveUI.Validation.Formatters;
 using ReactiveUI.XamForms;
 using Xamarin.Forms.Xaml;
 
@@ -11,7 +15,20 @@ namespace Camelotia.Presentation.Xamarin.Views
         public RenameFileView()
         {
             InitializeComponent();
-            this.WhenActivated(disposables => { });
+            this.WhenActivated(disposables =>
+            {
+                this.BindValidation(ViewModel, x => x.NewName, x => x.FileNameErrorLabel.Text)
+                    .DisposeWith(disposables);
+                this.BindValidation(ViewModel, x => x.FormErrorLabel.Text, new SingleLineFormatter(Environment.NewLine))
+                    .DisposeWith(disposables);
+
+                this.WhenAnyValue(x => x.FileNameErrorLabel.Text, text => !string.IsNullOrWhiteSpace(text))
+                    .BindTo(this, x => x.FileNameErrorLabel.IsVisible)
+                    .DisposeWith(disposables);
+                this.WhenAnyValue(x => x.FormErrorLabel.Text, text => !string.IsNullOrWhiteSpace(text))
+                    .BindTo(this, x => x.FormErrorLabel.IsVisible)
+                    .DisposeWith(disposables);
+            });
         }
     }
 }
